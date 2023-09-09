a42ce293e6f8eb914121b2356015ec443fdf847c
Yuri Cavalcante <yuri.scavalcante@outlook.com>
2023-09-09 17:28:43 -0300

    feat(UserModel): User model

---
+++ src/app/models/user-model.ts
@@ -0,0 +1,8 @@
+export class UserModel {
+  full_name?: string = '';
+  email: string = '';
+  password: string = '';
+  confirm_password?: string = '';
+  course?: string = '';
+  class?: string = '';
+}
