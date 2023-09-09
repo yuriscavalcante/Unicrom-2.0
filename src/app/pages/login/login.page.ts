192afb645991c38bd1c2dc7dd313ce3ab5f928d5
Yuri Cavalcante <yuri.scavalcante@outlook.com>
2023-09-09 17:29:03 -0300

    feat(login): Login page

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { presentToast } from 'src/app/components/shared/toast';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  loginForm: FormGroup;
+  submitted = false;
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', [Validators.minLength(8), Validators.required]],
+    });
   }
 
+  ngOnInit() {}
+
+  async onSubmit() {
+    if (this.loginForm.valid) {
+      (await presentToast('Teste', 1500, 'bottom', 'success')).present();
+    } else {
+      (
+        await presentToast(
+          'Email e/ou senha invalidos',
+          1500,
+          'bottom',
+          'danger'
+        )
+      ).present();
+    }
+  }
 }
